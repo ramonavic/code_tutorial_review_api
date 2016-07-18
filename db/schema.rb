6134a12480ae9c258fae30f610abda88bacf241d
ramonavic <ramon.hofmeister@gmail.com>
2016-07-18 18:18:44 +0200

    tutorial validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718155302) do
+ActiveRecord::Schema.define(version: 20160718161600) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160718155302) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "link"
   end
 
   add_index "tutorials", ["user_id"], name: "index_tutorials_on_user_id"
--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe Tutorial, type: :model do
   describe "validation" do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to have_many(:reviews) }
+    it { is_expected.to validate_presence_of(:user) }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:link) }
 
   end
 end
