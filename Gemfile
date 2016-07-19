5c10f5136fb074e0864e77b2b3f4557e8e8e126f
ramonavic <ramon.hofmeister@gmail.com>
2016-07-19 17:29:44 +0200

    clean up

--- Gemfile
+++ Gemfile
@@ -26,7 +26,6 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'capybara'
 
-gem "factory_girl_rails", "~> 4.0"
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,10 +31,7 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.include Warden::Test::Helpers
-  config.before :suite do
-    Warden.test_mode!
-  end
+
 
   config.before(:suite) do
     DatabaseCleaner[:active_record].strategy = :transaction
