447720f3cd703a837537f33f3206a6bfb8b618ae
ramonavic <ramon.hofmeister@gmail.com>
2016-07-19 11:46:01 +0200

    tutorial controller tests and routes

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe Tutorial, type: :model do
   end
 
   describe "validations" do
-
     it { should validate_presence_of(:title)}
     it { should validate_presence_of(:link)}
     it { should validate_presence_of(:description)}
