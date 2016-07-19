a0372c155e9c71f7eda64ea4d108daea5b61021b
ramonavic <ramon.hofmeister@gmail.com>
2016-07-19 09:50:14 +0200

    review validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :tutorial
+  validates :user, presence: true
+  validates :rating, presence: true
+
+
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,5 +3,8 @@ require 'rails_helper'
 RSpec.describe Review, type: :model do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to belong_to(:tutorial) }
+  it { is_expected.to validate_presence_of(:user) }
+  it { is_expected.to validate_presence_of(:rating) }
+
 
 end
