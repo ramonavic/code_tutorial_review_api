a0fc6c7ee8ad9f81110792522f378cd102f37937
ramonavic <ramon.hofmeister@gmail.com>
2016-07-21 10:13:10 +0200

    cors app edits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
-  acts_as_token_authentication_handler_for User
   protect_from_forgery with: :null_session
+  acts_as_token_authentication_handler_for User
   skip_before_action :verify_authenticity_token
 
   before_action :authenticate_user!
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,5 +1,5 @@
 class TutorialReviewApi::Application
-  config.middleware.insert_before Warden::Manager,  "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
+  config.middleware.insert_before 0,  "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
     allow do
       origins 'localhost:3000', '127.0.0.1:3000'
 
@@ -11,7 +11,7 @@ class TutorialReviewApi::Application
 
       resource '*',
         headers: :any,
-        methods: [:get, :post, :delete, :put, :patch, :options, :head],
+        methods: [:get, :delete, :put, :patch, :options, :head],
         max_age: 0
     end
   end
