faf8a29f235dd76ce502060dc16fbc7549bba228
ramonavic <ramon.hofmeister@gmail.com>
2016-07-21 13:36:55 +0200

    session store current user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,12 @@
 class SessionsController < Devise::SessionsController
     respond_to :json
-end  
+
+  def create
+    if user = User.authenticate(params[:username], params[:password])
+      # Save the user ID in the session so it can be used in
+      # subsequent requests
+      session[:current_user_id] = user.id
+      redirect_to root_url
+    end
+  end
+end
