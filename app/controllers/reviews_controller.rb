1145f5f9b7e654f7efc7203071e2e921694e5f61
ramonavic <ramon.hofmeister@gmail.com>
2016-07-25 16:16:35 +0200

    show and create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,6 +5,30 @@ class ReviewsController < ApplicationController
     @reviews = Review.all
   end
 
+  def show
+    @review = Review.find(params[:id])
+    render json: {review: @review}
+  end
+
+  def create
+    @review.tutorial_id = Tutorial.find(params[:tutorial_id])
+    @review.user = current_user
+    @review = Review.new(tutorial: tutorial, user: user)
+
+    if @tutorial.save
+      render json: @review, status: :created
+    else
+      render json: @review
+    end
+  end
+
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :user, :tutorial)
+  end
 
 
 end
--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ReviewsController, type: :controller do
   let (:user) {User.create(email: "testwoman@testing.com", password: "abcd1234" )}
-  let (:tutorial) {Tutorial.create(title: "How to get mad RSpec", description: "Follow these steps to be a testing genius", link: "www.rspec.com", user: user)}
+  let (:tutorial) {Tutorial.create(title: "How to get mad RSpec", description: "Follow these steps to be a testing genius", link: "www.rspec.com", user: user, id: 2313)}
 
 
   let (:valid_attributes) do
@@ -39,14 +39,44 @@ RSpec.describe ReviewsController, type: :controller do
         expect(response).to be_success
         expect(response.content_type).to eq "application/json"
       end
-
     end
-
-
-
   end
 
+  describe "GET #show" do
+    it "is not accesible when not logged in" do
+      get :show, {id: valid_review.to_param}, format: :json
+      expect(response).to redirect_to new_user_session_path
+    end
 
+  context "when logged in" do
+    login_user
+
+    it "assigns the requested review as @review" do
+      get :show, {id: valid_review.to_param}, format: :json
+      expect(response).to be_success
+      expect(assigns(:review)).to eq(valid_review)
+      end
+    end
+  end
+
+  describe "POST #create" do
+    it "doesn't allow a user to post when not logged in" do
+      post :create, {id: valid_review.to_param}, format: :json
+      expect(response).to redirect_to new_user_session_path
+  end
+
+    context "when logged in" do
+      login_user
+
+      it "posts the created review as @review" do
+
+      post :create, {review: {id: valid_review.to_param}, format: :json}
+      expect(response).to have_http_status(:created)
+      expect(response.content_type).to eq "application/json"
+
+      end
+    end
+  end
 
 
 
